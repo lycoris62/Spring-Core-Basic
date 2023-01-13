71c7c42c93f518b5eb6c0b43db4c9f07ed81d6d9
Lycoris62 <wodbs7758@naver.com>
2023-01-13 15:14:34 +0900

    [섹션 7] 의존관계 자동 주입 : 롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // final 로 생성자에서만 초기화 하여 불변, 누락을 막음.
@@ -16,13 +18,14 @@ public class OrderServiceImpl implements OrderService{
 //    @Autowired private MemberRepository memberRepository;
 //    @Autowired private DiscountPolicy discountPolicy;
 
-    @Autowired // 생성자가 1개 있다면 @Autowired 생략해도 자동 주입됨.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // @RequiredArgsConstructor 으로 final 붙은 멤버변수들을 생성자 자동 생성해주어 생략 가능.
+//    @Autowired // 생성자가 1개 있다면 @Autowired 생략해도 자동 주입됨.
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        System.out.println("discountPolicy = " + discountPolicy);
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 //    @Autowired // final 없애고 수정자(setter)로 바꿀 수 있음.
 //    public void setMemberRepository(MemberRepository memberRepository) {
