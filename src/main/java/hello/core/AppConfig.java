adf591cf3d815a1c1ffcaf2b1f37b33375750034
Lycoris62 <wodbs7758@naver.com>
2023-01-13 10:49:01 +0900

    first commit

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,53 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    // 예상:
+    // 1. Call AppConfig.memberService
+    // 2. Call AppConfig.memberRepository
+    // 3. Call AppConfig.memberRepository
+    // 4. Call AppConfig.orderService
+    // 5. Call AppConfig.memberRepository
+
+    // 실제:
+    // 1. Call AppConfig.memberService
+    // 2. Call AppConfig.memberRepository
+    // 3. Call AppConfig.orderService
+
+    @Bean
+    public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("Call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
